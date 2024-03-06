bc9f1124ce1ce636e4a44ae69eaf1592dbf671a5
Chaunghi2508 <145953746+nhocti2106@users.noreply.github.com>
2024-03-06 09:35:17 +0700

    Create baitaprutgon.js

---
+++ baitaprutgon.js
@@ -0,0 +1,77 @@
+var sach=[
+    {
+        tacgia: 'Harper Lee',
+        tieuDe: 'To Kill a Mockingbird',
+        namXuatBan: 2020,
+        trangthai: true
+    },
+    {
+        tacgia: 'Ray Bradbury',
+        tieuDe: 'Fahrenheit 451',
+        namXuatBan: 2022,
+        trangthai: false
+    }
+]
+for ( i in sachs) {
+console.log(
+${i} Tieu De: ${sachs [i] .tieude}
+Tac gia: ${sachs [i] .tacgia)
+Nam xuat ban: ${sachs [i].namxuatban)}
+Trang thai: ${sachs [i]. trangthai? 'con': 'het'}
+);
+
+
+Let number-[2, 5,7,8,9]:
+Let max = number [];
+Let min = number[o];
+for (var i = 0; i < number.length; it+){
+// if (max <number[i]){
+1/
+max=number [i];
+1/}
+(max < number [i]) ? max-number [i] : max;
+1/ if(min>number [i]){
+1/ min=number [i];
+(min > number [i] ) ? min=number [i]: min ;
+}
+console. log ("so lon nhat", max) ;
+console.log( "so nho nhat", min);
+
+https://drive.google.com/file/d/1WusXWtLVjOA3qNO2Gk8CKTxvNtHE7COh/view?usp=sharing
+Let number-[2, 5,7,8,9]:
+Let max = number [];
+Let min = number[o];
+for (var i = 0; i < number.length; it+){
+// if (max <number[i]){
+1/
+max=number [i];
+1/}
+(max < number [i]) ? max-number [i] : max;
+1/ if(min>number [i]){
+1/ min=number [i];
+(min > number [i] ) ? min=number [i]: min ;
+}
+console. log ("so lon nhat", max) ;
+console.log( "so nho nhat", min);
+function FindkeyLong (name) {
+name.split (' ');
+Let ten
+Let max = ten[];
+for(Let i = 0; i<ten. length; i++){
+if(max< ten[i]){
+max = ten [i];
+}
+console.log (max) ;
+}
+FindKeyLong ('Nguyen Da Da');
+function
+FindkeyLong2 (name) {
+Let ten
+Let
+max
+name.trim() . split (" "). forEach (element => {
+max<=el ement . length ? (ten = element , max = element .length):
+ten
+= ten
+console. log (ten);
+FindkeyLong2 (' Nguyen rgzdhdth Da');
